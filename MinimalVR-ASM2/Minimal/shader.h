2786edf5fdd6e58ead64d07ef07c5285801d0e15
yuz100086 <yuz287@ucsd.edu>
2017-05-03 10:53:01 -0700

    uv

--- MinimalVR-ASM2/Minimal/shader.h
+++ MinimalVR-ASM2/Minimal/shader.h
@@ -8,13 +8,13 @@
 
 #include <GL/glew.h>
 
-class Shader
+class MyShader
 {
 public:
 	GLuint Program;
 	// Constructor generates the shader on the fly
-	Shader() {}
-	Shader(const GLchar* vertexPath, const GLchar* fragmentPath)
+	MyShader() {}
+	MyShader(const GLchar* vertexPath, const GLchar* fragmentPath)
 	{
 		// 1. Retrieve the vertex/fragment source code from filePath
 		std::string vertexCode;
